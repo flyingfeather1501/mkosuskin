82c189f01538c27ea960d81ca2e0b5641640ff16
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-05-27 20:02:53 +0800

    Restructure. Blends are now in src/, imouji_pack.sh does a full render / compile into an osk. Also edited ranking-graph, spinner-middle2, cursortrail, and added spinner-top. Closes #3, closes #2, closes #9, closes #8.

--- 0.imouji_pack.sh
+++ 0.imouji_pack.sh
@@ -1,54 +1,11 @@
 #!/bin/bash
-# Still missing:
-### Image
-# - cursor.png cursormiddle.png cursortrail.png
-# - followpoint-n.png
-# - inputoverlay-background.png inputoverlay-key.png
-# - menu-back-n.png menu-button-background.png
-# - play-skip.png
-# - ranking-panel.png
-# - ready.png
-# - scorebar-bg.png scorebar-colour-n.png
-# - section-fail.png section-pass.png
-# - selection-tab.png
-# - spinner-rpm.png
-# - star.png
-# - star2.png
-# - mania, taiko, catch
 
-empties=(lighting.png sliderendcircle.png sliderpoint10.png sliderpoint30.png sliderscorepoint.png spinner-{glow,middle,bottom,clear,osu,top}.png ranking-graph.png mode-{fruits,taiko,mania,osu}-small.png hit300{,g,k}-0.png count{1,2,3}.png)
+### functions
+render_marker () {
+  blender -b $1 --python render_marker.py
+}
 
-echo creating empty images...
-for i in ${empties[@]}; do
-  convert -size 1x1 xc:none $i
-done
-
-if ls *.png 2>/dev/null >/dev/null; then
-  echo trimming totrim images
-  for i in *totrim.png; do
-    [ ! -f $i ] && continue
-    convert -trim +repage $i $(basename $i totrim.png).png
-    rm $i
-  done
-fi
-
-
-echo copying button.png into button-*
-for i in button-{left,middle,right}.png; do
-  cp button.png $i
-done
-rm button.png
-
-#echo creating score-percent...
-#convert -size 1040x1 xc:none score-percent.png
-
-echo resizing ranking-small...
-for i in ranking-{A,B,C,D,S,SH,X,XH}@2xtmp.png; do
-  [ ! -f $i ] && continue
-  convert -resize 8% $i $(basename $i @2xtmp.png)-small@2x.png
-  rm $i
-done
-
+alldownto2x () {
 for f in 8 4; do
   echo resizing @"$f"x to @$((f/2))x and removing @"$f"x...
   for i in *@"$f"x.png; do
@@ -57,28 +14,75 @@ for f in 8 4; do
     rm $i
   done
 done
+}
 
+generate_empties () {
+  echo creating empty images...
+  for i in ${empties[@]}; do
+    convert -size 1x1 xc:none $i
+  done
+}
+
+HD2SD () {
+  echo creating SD images from @2x...
+  for i in *@2x.png; do
+    convert -resize 50% $i $(basename $i @2x.png).png
+  done
+}
+
+autotrim () {
+  echo trimming totrim images
+  for i in *totrim.png; do
+    [ ! -f $i ] && continue
+    convert -trim +repage $i $(basename $i totrim.png).png
+    rm $i
+  done
+}
+
+autoresize () {
+  echo resizing images with _resizeto in them
+  # name the files as ${name}_resizeto${x}x${y}.png
+  file=$(find *resizeto*); [[ $? != 0 ]] && exit
+  size=$(echo $file | cut -d'_' -f 2 | sed 's/resizeto//g; s/\.png//')
+  convert -resize $size $file $(basename $file _resizeto"$size".png)
+  rm $file
+}
+
+### prepare
+version=$(date +%Y%m%d%H%M%S%z)
+skinname="ReZero Script"
+out=out/"${skinname}"-"${version}"
+projectroot="$(pwd)"
+mkdir -p "$out"
+
+cd "$projectroot"/src/
+rm *.png # Cleanup
+
+### render images
+empties=(lighting.png sliderendcircle.png sliderpoint10.png sliderpoint30.png sliderscorepoint.png spinner-{glow,middle,bottom,clear,osu}.png ranking-graph.png hit300{,g,k}-0.png count{1,2,3}.png)
+
+gen_empties
+render_marker *.blend
+autoresize
+alldownto2x
 echo resizing score-dot and score-comma...
 for i in score-{dot,comma}@2xtmp.png; do
   [ ! -f $i ] && continue
   convert -crop 20x84+14+0 $i $(basename $i @2xtmp.png)@2x.png
   rm $i
 done
+autotrim
+HD2SD
 
-echo resizing inputoverlay-key...
-for i in inputoverlay-key@2xtmp.png; do
-  [ ! -f $i ] && continue
-  convert -resize 86x86 $i $(basename $i @2xtmp.png)@2x.png
-  rm $i
-done
+cp button-left.png button-middle.png
+cp button-left.png button-right.png
 
-echo creating SD images from @2x...
-for i in *@2x.png; do
-  convert -resize 50% $i $(basename $i @2x.png).png
-done
+### package
+cd "$projectroot"
+mv src/*.png "$out"/
+cp Audio/* "$out"/ # gotta also manage audio files later
+cp External\ Audio/* "$out"/
+sed "s/NNNNAAAAMMMMEEEE/$skinname/g" src/skin.ini > "$out/skin.ini"
 
-echo copying combo-{0-9} to default-{0-9}...
-for i in $(ghci -e [0..9] | sed 's/,/ /g' | sed 's/\[//g' | sed 's/\]//g'); do
-  cp combo-"$i"@2x.png default-"$i"@2x.png
-  cp combo-"$i".png default-"$i".png
-done
+7z a "$out".zip "$out"
+mv "$out".zip "$out".osk
