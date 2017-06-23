f9378a168280842455b82bc0dc5f9c5f38be6aad
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-06-23 22:34:34 +0800

    Organize variables & functions; move matching out of autotrim; render_marker->render_python which accepts blend & python script

--- 0.imouji_pack.sh
+++ 0.imouji_pack.sh
@@ -2,157 +2,62 @@
 ## Fancy way to render an osu skin.
 ## Requires: blender, lmms, p7zip, parallel, vips, imagemagick
 
-## functions
+## initialize
+trap 'cleanup aborted; exit 1' INT
+OPTIND=1
 
-exithelp () {
-  echo "Usage: $(basename $0) [-r REVISION | -d] [-h|--help]"
-  echo "Arguments:"
-  echo " -r REVISION  specify a revision"
-  echo " -d           revision=dev"
-  echo " -h           print help (this message)"
-  exit $1
-}
-
-echoreport () {
-  echo ${BOLD}${MAGENTA}"<$0> "${NORMAL}${BOLD}"$*"${NORMAL}
-}; export -f echoreport
-
-echoerror () {
-  echo ${BOLD}${MAGENTA}"<$0> "${RED}${BOLD}"$*"${NORMAL} 1>&2
-}; export -f echoerror
-
-cleanup () {
-  echoerror aborted by user, cleaning up...
-  rm "$projectroot"/src/*.png 2>/dev/null
-  for i in "$projectroot"/out/"$outname"{,.osk,.zip}; do
-    [ -f "$i" ] && rm "$i" -r
-  done
-  echoreport exiting...
-  exit
-}
-
-render_normal () {
-  echoreport rendering "$1"...
-  blender -b "$1" -a >/dev/null
-}; export -f render_normal
-
-render_marker () {
-  echoreport rendering "$1" with render_marker.py ...
-  blender -b "$1" --python "$projectroot"/utils/render_marker.py >/dev/null
-}; export -f render_marker
-
-render_audio_lmms () {
-  echoreport rendering audio "$1" ...
-  lmms --format wav -r "$1" >/dev/null
-  mv "$(basename $1 .mmpz)".wav "$(echo $(basename $1 .mmpz) | sed 's/^lmms\.//g').wav"
-}; export -f render_audio_lmms
-
-#alldownto2x () {
-#for f in 8 4; do
-#  echoreport resizing @"$f"x to @$((f/2))x and removing @"$f"x...
-#  parallel
-#  for i in *@"$f"x.png; do
-#    [ ! -f $i ] && continue
-#    convert -resize 50% $i "$(basename $i @"$f"x.png)@$((f/2))x.png"
-#    rm $i
-#  done
-#done
-#}
-
-#HD2SD () {
-#  echo resizing $1 ...
-#  convert -resize 50% $1 "$(basename $1 @2x.png).png"
-#}; export -f HD2SD
-
-autotrim () {
-  echoreport trimming totrim images...
-  for i in *totrim.png; do
-    [ ! -f $i ] && continue
-    convert -trim +repage $i "$(basename $i totrim.png).png"
-    rm $i
-  done
-}
-
-resize_at () {
-  # resize_at <n|t> <stst@Nx.png>
-  # $1 == n -> only resize @n | n != 2
-  # $1 == t -> only resize @2
-  two_switch=$1
-  orig_file=$2
-  orig_size=$(echo $orig_file | sed 's/.*@//g; s/x.*png//g') # "*@3x.png" -> "3x.png" -> "3"
-  case $two_switch in
-    t)
-      [ ! $orig_size -eq 2 ] && return
-      target_size=1
-      target_file=$(basename "$orig_file" @2x.png).png
-      ;;
-    n)
-      [ $orig_size -eq 2 ] && return
-      target_size=2
-      target_file=$(basename "$orig_file" @"$orig_size"x.png)@"$target_size"x.png
-      ;;
-    *)
-      echoerror resize_at failed && exit
-      ;;
-  esac
-    echo resizing $orig_file...
-  #convert -resize $(python -c "print('{0:.2f}'.format(${target_size} / ${orig_size}))") $orig_file $target_file
-  vips resize $orig_file $target_file $(python -c "print('{0:.2f}'.format(${target_size} / ${orig_size}))")
-  [ ! "$orig_size" -eq 2 ] && rm $orig_file
-}; export -f resize_at
-
-resize_resizeto () {
-  echo resizing $1...
-  # name the files as ${name}_resizeto${x}x${y}.png
-  orig_file="$1"
-  size=$(echo $orig_file | cut -d'_' -f 2 | sed 's/resizeto//g; s/\.png//')
-  target_file="$(basename $orig_file _resizeto"$size".png).png"
-  convert -resize $size $orig_file $target_file
-  #vipsthumbnail --size="$size" -o $target_file $orig_file
-  rm $1
-}; export -f resize_resizeto
-
-## prepare
 export BOLD=$(tput bold)
 export NORMAL=$(tput sgr0)
 export RED=$(tput setaf 1)
 export MAGENTA=$(tput setaf 5)
-trap cleanup INT
-case $1 in
-  -r)
-    if [ -z $2 ]; then
-      exithelp 1
-    else
-      revision="$2"
-    fi
-    ;;
-  -d)
-    revision="dev"
-    ;;
-  -h|--help)
-    exithelp 0
-    ;;
-  *)
-    revision=$(date +%Y%m%d%H%M%S%z)
-esac
-export skinname="ReZero Script"
-export outname="${skinname} ${revision}"
+
 export projectroot="$(pwd)"
-mkdir -p "$projectroot"/out/"$outname"
+export utils_dir="$projectroot"/utils # render_marker, build_functions, etc.
+export build_dir="$projectroot"/out # where each version's output sits
+export source_dir="$projectroot"/src # .blend, .mmpz, .svg, etc.
+export skinname="ReZero Script"
 
-cd "$projectroot"/src/
-rm *.png >/dev/null 2>/dev/null # Cleanup
+source "$utils_dir"/utils.bash
 
-echoreport starting to render "$outname"...
+## get options
+while getopts "r:dh" opt; do
+  case $opt in
+    r)
+      revision="$OPTARG"
+      ;;
+    d)
+      revision="dev"
+      ;;
+    h)
+      exithelp 0
+      ;;
+    *)revision=$(date +%Y%m%d%H%M%S%z)
+  esac
+done
+
+export outname="${skinname} ${revision}"
+mkdir -p "$build_dir"/"$outname"
+
+## prepare
+echoreport cleaning up...
+cleanup
+
+cd "$source_dir"
+echoreport start rendering "$outname"...
 
 ## render images
-empties=(count1.png count2.png count3.png default-{0..9}.png hit300-0.png hit300g-0.png hit300k-0.png inputoverlay-background.png lighting.png ranking-graph.png scorebar-bg.png sliderendcircle.png sliderpoint10.png sliderpoint30.png sliderscorepoint.png spinner-bottom.png spinner-clear.png spinner-glow.png spinner-middle.png star2.png)
+empties=(count{1..3}.png default-{0..9}.png \
+hit300{,g,k}-0.png inputoverlay-background.png lighting.png \
+ranking-graph.png scorebar-bg.png sliderendcircle.png \
+sliderpoint10.png sliderpoint30.png sliderscorepoint.png \
+spinner-bottom.png spinner-clear.png spinner-glow.png spinner-middle.png \
+star2.png)
 
 ### empties
 echoreport copying empty image template to images...
 parallel 'cp empty_image' ::: ${empties[*]}
 
-parallel render_marker ::: rendermarker.*.blend
+parallel render_python {} "$utils_dir"/render_marker.py ::: rendermarker.*.blend
 parallel render_normal ::: rendernormal.*.blend
 parallel render_audio_lmms ::: lmms.*.mmpz
 
@@ -163,7 +68,7 @@ for i in score-{dot,comma}@2xtmp.png; do
   convert -crop 20x84+14+0 $i "$(basename $i @2xtmp.png)@2x.png"
   rm $i
 done
-autotrim
+parallel autotrim ::: *totrim.png
 
 ### resize
 echoreport resizing other images...
@@ -177,13 +82,13 @@ cp button-left.png button-right.png
 ## package
 cd "$projectroot"
 echoreport moving rendered files into output folder...
-mv src/*.png out/"$outname"/
-mv src/*.wav out/"$outname"/
-cp audio/*.ogg out/"$outname"/ # for external / prerecorded audio files
+mv "$source_dir"/*.png "$build_dir"/"$outname"/
+mv "$source_dir"/*.wav "$build_dir"/"$outname"/
+cp audio/*.ogg "$build_dir"/"$outname"/ # for external / prerecorded audio files
 sed "s/NNNNAAAAMMMMEEEE/$skinname $revision/g" src/skin.ini > out/"$outname"/skin.ini
 
 echoreport packaging output folder into osk file...
-cd $projectroot/out
+cd "$build_dir"
 7z a "$outname".zip "$outname"/
 mv "$outname".zip "$outname".osk
 
---
+++ utils/utils.bash
@@ -0,0 +1,86 @@
+exithelp () {
+  echo "Usage: $(basename $0) [-r REVISION | -d] [-h|--help]"
+  echo "Arguments:"
+  echo " -r REVISION  specify a revision"
+  echo " -d           revision=dev"
+  echo " -h           print help (this message)"
+  exit $1
+}
+
+echoreport () {
+  echo ${BOLD}${MAGENTA}"<$0> "${NORMAL}${BOLD}"$*"${NORMAL}
+}; export -f echoreport
+
+echoerror () {
+  echo ${BOLD}${MAGENTA}"<$0> "${RED}${BOLD}"$*"${NORMAL} 1>&2
+}; export -f echoerror
+
+cleanup () {
+  # cleanup [aborted]
+  [ "$1" == "aborted" ] && echoerror aborted by user, cleaning up...
+  rm "$source_dir"/*.png 2>/dev/null
+  for i in "$build_dir"/"$outname"{,.osk,.zip}; do
+    [ -f "$i" ] && rm "$i" -r
+  done
+}
+
+render_normal () {
+  echoreport rendering "$1"...
+  blender -b "$1" -a >/dev/null
+}; export -f render_normal
+
+render_python () {
+  echoreport rendering "$1" with python script "$(basename $2)" ...
+  blender -b "$1" --python "$2" >/dev/null
+}; export -f render_python
+
+render_audio_lmms () {
+  echoreport rendering audio "$1" ...
+  lmms --format wav -r "$1" >/dev/null
+  mv "$(basename $1 .mmpz)".wav "$(echo $(basename $1 .mmpz) | sed 's/^lmms\.//g').wav"
+}; export -f render_audio_lmms
+
+autotrim () {
+  echoreport trimming totrim images...
+  convert -trim +repage $1 "$(basename $1 totrim.png).png"
+  rm $1
+}
+
+resize_at () {
+  # resize_at <n|t> <stst@Nx.png>
+  # $1 == n -> only resize @n | n != 2
+  # $1 == t -> only resize @2
+  two_switch=$1
+  orig_file=$2
+  orig_size=$(echo $orig_file | sed 's/.*@//g; s/x.*png//g') # "*@3x.png" -> "3x.png" -> "3"
+  case $two_switch in
+    t)
+      [ ! $orig_size -eq 2 ] && return
+      target_size=1
+      target_file=$(basename "$orig_file" @2x.png).png
+      ;;
+    n)
+      [ $orig_size -eq 2 ] && return
+      target_size=2
+      target_file=$(basename "$orig_file" @"$orig_size"x.png)@"$target_size"x.png
+      ;;
+    *)
+      echoerror resize_at failed && exit
+      ;;
+  esac
+    echo resizing $orig_file...
+  #convert -resize $(python -c "print('{0:.2f}'.format(${target_size} / ${orig_size}))") $orig_file $target_file
+  vips resize $orig_file $target_file $(python -c "print('{0:.2f}'.format(${target_size} / ${orig_size}))")
+  [ ! "$orig_size" -eq 2 ] && rm $orig_file
+}; export -f resize_at
+
+resize_resizeto () {
+  echo resizing $1...
+  # name the files as ${name}_resizeto${x}x${y}.png
+  orig_file="$1"
+  size=$(echo $orig_file | cut -d'_' -f 2 | sed 's/resizeto//g; s/\.png//')
+  target_file="$(basename $orig_file _resizeto"$size".png).png"
+  convert -resize $size $orig_file $target_file
+  #vipsthumbnail --size="$size" -o $target_file $orig_file
+  rm $1
+}; export -f resize_resizeto
