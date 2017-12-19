4ada9545103c3eb71b59d3fce22b4563c90ca635
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-19 18:06:35 +0900

    add path-basename function

--- helper.rkt
+++ helper.rkt
@@ -5,6 +5,8 @@
 
 (define (path-replace path from to #:all [all? #t])
   (string->path (string-replace (path->string path) from to #:all all?)))
+(define (path-basename path)
+  (path-replace path #rx".*/" ""))
 
 (define (path-contains? path contained)
   (string-contains? (path->string path) contained))
